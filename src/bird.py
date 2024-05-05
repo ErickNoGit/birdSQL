073244b95a5e530222a6bf96b3f04ce66c2e8c7f
Erick Alves <one.job.win@outlook.com>
2024-05-05 12:37:30 -0300

    Refactored stack_func and filepath_system

--- src/bird.py
+++ src/bird.py
@@ -6,29 +6,41 @@ from typing import Union
 
 class ExceptMethod:
     """Customized error log for each method"""
+    def __init__(self) -> None:
+        pass
     
-    def stack_func(self, error: Union[Exception, ValueError], class_: str) -> str:
+    def stack_func(self, class_: str, error: Exception) -> str:
         """Use an error to return a function"""
-        frame_ = inspect.stack()[1]
-        return f'Error method -> "{class_ + '.' + frame_.function}": {error}'
+        err = f'"{class_ + '.' + inspect.stack()[1].function}": {error}'
+        return f'Error method -> {err}'
 
 
 class BirdSystem(ExceptMethod):
     """Controls data types for operating systems"""
-
     def __init__(self) -> None:
         super().__init__()
 
 
-    def filepath_siystem(self, file: Union[str, PathLike]) -> str:
-        """returns the absolute path of a file according to the operating system"""
+    def filepath_system(self, file: Union[str, PathLike]) -> str:
+        """
+        returns the absolute path of a file according to the operating system
+
+        Args:
+            `file`: File path
+        Returns:
+            `str`: Absolute file path
+        Raises:
+            - ValueError
+                - File path not found.
+
+            - Exception
+                - `ExceptMethod.stack_fun`: Pointer to the above error
+        """
         try:
             if not path.exists(file):
                 raise ValueError(f'File "{file}" not found')
 
-            plat_ = platform.system()
-
-            if plat_ == 'Windows':
+            if platform.system() == 'Windows':
                 return str(path.abspath(file)).replace("/", "\\")
         except Exception as err:
             class_name = str(self.__class__.__name__)
@@ -38,5 +50,5 @@ class BirdSystem(ExceptMethod):
 if __name__ == '__main__':
     bs = BirdSystem()
     arq_ = './src/config/basic_conecfeast.json'
-    path_like = bs.filepath_siystem(file=arq_)
+    path_like = bs.filepath_system(file=arq_)
     print(path_like)
