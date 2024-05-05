763afd5e4eb56455d4d5e6332fcbf5a5c560bda8
Erick Alves <one.job.win@outlook.com>
2024-05-05 20:46:11 -0300

    refactored connection and new disconnection functions

--- src/bird_mysql.py
+++ src/bird_mysql.py
@@ -1,8 +1,18 @@
+# Python 3.12.3
+import os
+import sys
 import json
 import mysql.connector
-from os import path, PathLike
-from typing import Tuple, Union
-from .bird import BirdSystem
+from os import PathLike
+from pathlib import Path
+from typing import Union
+from mysql.connector.cursor_cext import CMySQLCursor 
+from mysql.connector.connection_cext import CMySQLConnection
+
+src = os.path.dirname(Path(__file__))
+sys.path.append(src)
+
+from bird import BirdSystem
 
 
 class BirdMySQL(BirdSystem):
@@ -20,8 +30,9 @@ class BirdMySQL(BirdSystem):
             'data_base': None
         }
 
-        self.obj_conect = None
-        self.cursor_ = None
+        self.__debug_conn = None
+        self.__conn = None
+        self.__cursor_ = None
 
 
     def conn_mysql(self, con: Union[PathLike, dict] = None, **kwargs) -> bool:
@@ -35,25 +46,21 @@ class BirdMySQL(BirdSystem):
             `bool`: True if the connection was successful otherwise None
         Raises:
             - ValueError
-                - File path not found.
                 - You forgot to pass arguments.
                 - Forgot a parameter
                 - The key is empty.
 
             - Exception
-                - `ExceptMethod.stack_fun`: Pointer to the above errors
+                - `BirdSystemError.stack_fun`: Pointer to the above errors
         """
         try:
             if isinstance(con, str):
                 arq_ = self.filepath_system(con)
-                if 'Error method' in arq_:
-                    raise ValueError(f'File "{con}" not found')
-
                 with open(arq_, 'r') as file_json:
                     con = dict(json.load(file_json))
 
-            if bool(kwargs):
-                con = kwargs
+            if kwargs:
+                con.update(kwargs)
 
             if not bool(con):
                 raise ValueError('You forgot to pass arguments')
@@ -71,40 +78,68 @@ class BirdMySQL(BirdSystem):
                     raise ValueError(f'The "{k}" key is empty')
 
             if 4 == qtd_keys and ('data_base' in keys_dic):
-                conn = mysql.connector.connect(
+                self.__conn = mysql.connector.connect(
                     host=con['host'],
                     user=con['user'],
                     password=con['pass_'],
                     database=con['data_base']
                 )
-                self.obj_conect = con
-                self.cursor_ = conn.cursor()
-
-                self.__BASE_CONECT['host'] = con['host']
-                self.__BASE_CONECT['user'] = con['user']
-                self.__BASE_CONECT['pass_'] = con['pass_']
-                self.__BASE_CONECT['data_base'] = con['data_base']
-                return True
             elif 3 == qtd_keys and ('data_base' not in keys_dic):
-                conn = mysql.connector.connect(
+                self.__conn = mysql.connector.connect(
                     host=con['host'],
                     user=con['user'],
                     password=con['pass_']
                 )
-                self.obj_conect = con
-                self.cursor_ = conn.cursor()
 
-                self.__BASE_CONECT['host'] = con['host']
-                self.__BASE_CONECT['user'] = con['user']
-                self.__BASE_CONECT['pass_'] = con['pass_']
-                return True
+            self.__cursor_ = self.__conn.cursor()
+            self.__debug_conn = con
+            self.__BASE_CONECT.update(con)
+            return True
+        except Exception as err:
+            print(self.stack_func(self.__CLASS_NAME, err))
+
+
+    def get_conn(self) -> dict:
+        """get connection dictionary"""
+        try:
+            conex = self.__conn.is_connected()
+            dicio_conex = isinstance(self.__debug_conn, dict)
+
+            if conex and dicio_conex:
+                return self.__debug_conn
+        except Exception as err:
+            err = 'You are connected ?' if 'NoneType' in str(err) else str(err)
+            print(self.stack_func(self.__CLASS_NAME, err))
+
+
+    def get_cursor(self) -> CMySQLCursor:
+        """get custom MySQL cursor"""
+        try:
+            if self.__conn.is_connected():
+                self.__cursor_ = self.__conn.cursor()
+                return self.__cursor_
+        except Exception as err:
+            err = 'You are connected ?' if 'NoneType' in str(err) else str(err)
+            print(self.stack_func(self.__CLASS_NAME, err))
+
+
+    def descon(self) -> None:
+        """Closes the connection to MySQL and the cursor"""
+        try:
+            if isinstance(self.__cursor_, CMySQLCursor):
+                self.__cursor_.close() 
+                self.__cursor_ = None
+
+            if isinstance(self.__conn, CMySQLConnection):
+                self.__conn.close()
+                self.__conn = None
         except Exception as err:
             print(self.stack_func(self.__CLASS_NAME, err))
 
 
 if __name__ == '__main__':
     bird_mysql = BirdMySQL()
-    src = path.dirname(__file__)
+    src = os.path.dirname(__file__)
 
-    json_ = path.join(src, 'config', 'ignore_test.json')
-    con = bird_mysql.conn_mysql(json_)
+    json_ = os.path.join(src, 'config', 'ignore_test.json')
+    bird_mysql.conn_mysql(json_)
